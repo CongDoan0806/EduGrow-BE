4563ce27cbcfa69c343d91f420eae48e859b0659
ToNga156 <ngaichi2004@gmail.com>
2025-05-11 12:13:41 +0700

    Commit-main-before-merge/nga

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Models;
-
-<<<<<<< be/feat/handle-login
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
