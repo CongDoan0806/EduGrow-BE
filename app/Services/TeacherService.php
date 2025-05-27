de9d630c648d06c3453205164df4633b312a686d
ToNga156 <ngaichi2004@gmail.com>
2025-05-27 15:13:05 +0700

    Show-teacher-class/nga

--- app/Services/TeacherService.php
+++ app/Services/TeacherService.php
@@ -22,12 +22,19 @@ class TeacherService
     {
         return $this->teacherRepository->getTeacherById($id);
     }
+
     public function createFeedback(array $data)
     {
         return $this->teacherRepository->createFeedback($data);
     }
+    
     public function getTags($teacherId){
         return $this->teacherRepository->getMentionByTeacher($teacherId);
+    }
+
+    public function getClassesByTeacherId($teacherId)
+    {
+        return $this->teacherRepository->getClassesByTeacherId($teacherId);
     }
 
    public function getDashboardData($teacherId)
